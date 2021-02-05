307962722ab3cd29dfcfb2456946f4062eb2cc0b
Connor Braeger <cjbraeger@gmail.com>
2021-02-04 21:02:50 -0500

    Added Get, Put, Delete, And Post functionality to PostController

--- SocialMedia.WebAPI/Controllers/PostController.cs
+++ SocialMedia.WebAPI/Controllers/PostController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,60 @@ namespace SocialMedia.WebAPI.Controllers
 {
     public class PostController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            var service = CreatePostService();
+            return Ok(service.GetPosts());
+        }
+        public IHttpActionResult Get(int id)
+        {
+            var service = CreatePostService();
+            return Ok(service.GetPostById(id));
+        }
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var service = CreatePostService();
+            if (!service.CreatePost(post))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+        public IHttpActionResult Put(PostEdit post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var service = CreatePostService();
+            if (!service.UpdatePost(post))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+
+
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePostService();
+            if(!service.DeletePost(id))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+
+
     }
 }
