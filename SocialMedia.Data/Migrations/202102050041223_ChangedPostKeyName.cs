a0b1b2201f02b777a662f51e4fca981e47dbc36e
Connor Braeger <cjbraeger@gmail.com>
2021-02-04 20:14:52 -0500

    Added post models and post services

---
+++ SocialMedia.Data/Migrations/202102050041223_ChangedPostKeyName.cs
@@ -0,0 +1,24 @@
+﻿namespace SocialMedia.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedPostKeyName : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.Post");
+            DropColumn("dbo.Post", "Id");
+            AddColumn("dbo.Post", "PostId", c => c.Int(nullable: false, identity: true));
+            AddPrimaryKey("dbo.Post", "PostId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Post", "Id", c => c.Int(nullable: false, identity: true));
+            DropPrimaryKey("dbo.Post");
+            DropColumn("dbo.Post", "PostId");
+            AddPrimaryKey("dbo.Post", "Id");
+        }
+    }
+}
--- SocialMedia.Models/PostDetail.cs
+++ SocialMedia.Models/PostDetail.cs
@@ -1,28 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SocialMedia.Data
+namespace SocialMedia.Models
 {
-    public class Post
+    public class PostDetail
     {
-        [Key]
         public int PostId { get; set; }
-        [Required]
-        public Guid Author { get; set; }
-        [Required]
         public string Title { get; set; }
-        [Required]
-        [MaxLength(280)]
         public string Text { get; set; }
-
-        [Required]
         public DateTimeOffset CreatedUtc { get; set; }
 
         public DateTimeOffset? ModifiedUtc { get; set; }
-
     }
 }
