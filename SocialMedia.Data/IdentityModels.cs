a87af53217993508a35c93c1f73a9dc2f23ff36b
Connor Braeger <cjbraeger@gmail.com>
2021-02-04 19:02:19 -0500

    Initialized database

--- SocialMedia.Data/IdentityModels.cs
+++ SocialMedia.Data/IdentityModels.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using SocialMedia.Data;
 
-namespace ElevenNote.Data
+namespace SocialMedia.Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -32,7 +33,7 @@ namespace ElevenNote.Data
         {
             return new ApplicationDbContext();
         }
-        //public DbSet<Post> Posts { get; set; }
+        public DbSet<Post> Posts { get; set; }
         //public DbSet<Comment> Comments { get; set; }
         //public DbSet<Reply> Replies { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
