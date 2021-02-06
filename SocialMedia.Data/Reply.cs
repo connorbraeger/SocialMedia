359dcf1a26d21f4732a9339db22e9f30d5143797
tuganbraden <tuganbraden@gmail.com>
2021-02-06 14:22:55 -0500

    functionality 2.0

--- SocialMedia.Data/Reply.cs
+++ SocialMedia.Data/Reply.cs
@@ -12,5 +12,24 @@ namespace SocialMedia.Data
         [Key]
         public int ReplyId { get; set; }
 
+        //[ForeignKey(nameof(Comment))]
+        //public int CommentId { get; set; }
+
+        //public virtual Comment Comment { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
     }
 }
--- SocialMedia.Models/ReplyEdit.cs
+++ SocialMedia.Models/ReplyEdit.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Models
 {
-    class ReplyEdit
+    public class ReplyEdit
     {
+        [Required]
+        public int ReplyId { get; set; }
+        public string Title { get; set; }
+
+        public string Text { get; set; }
     }
 }
